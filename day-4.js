b2641fb81d6d240da2bebfd27e7a823e334008db
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-06-19 23:40:54 -0300

    day 4 of algorithms

---
+++ day-4.js
@@ -0,0 +1,13 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'*/
+
+function solution(str){
+  return str.split("").reverse().join("");
+}
+
+console.log(solution('world'))
+console.log(solution('hello'))
+console.log(solution('h'))
+console.log(solution(''))
