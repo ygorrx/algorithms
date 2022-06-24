e91cea1f3bacbc46c0f1d75dec2d1a82f9168fd4
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-06-23 21:24:10 -0300

    day 8 algorithm

---
+++ day-8.js
@@ -0,0 +1,13 @@
+/* Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.*/
+
+function getCount(str) {
+ return Array.from(str).filter(letter => 'aeiou'.includes(letter)).length;
+ }
+ 
+
+
+console.log(getCount('abracadabra'))
